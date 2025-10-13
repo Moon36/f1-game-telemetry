1564400b3a1658e6dadf665bcdca507ec4657f2c
Moon36 <manuel-waibel@gmx.at>
2025-10-13 22:39:59 +0200

    Extract dashboard icon

--- src/dashboard/src/App.vue
+++ src/dashboard/src/App.vue
@@ -16,24 +16,7 @@
         </span>
       </span>
       <h1 class="text-2xl font-bold text-gray-100 flex items-center mb-2 sm:mb-0">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="28"
-          height="28"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide mr-2"
-        >
-          <rect width="18" height="18" x="3" y="3" rx="2" ry="2"></rect>
-          <line x1="3" x2="21" y1="9" y2="9"></line>
-          <line x1="3" x2="21" y1="15" y2="15"></line>
-          <line x1="9" x2="9" y1="3" y2="21"></line>
-          <line x1="15" x2="15" y1="3" y2="21"></line>
-        </svg>
+        <img src="/assets/icons/Dashboard_Icon.svg" alt="Dashboard Icon" class="w-7 h-7 mr-3" />
         F1 Telemetry Dashboard
       </h1>
 
