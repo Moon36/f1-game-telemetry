a40f2981536f032c20cb9ea7058869c99dabd901
Manuel Waibel <manuel-waibel@gmx.at>
2026-06-09 10:21:57 +0200

    Export common colors to separate file

---
+++ src/dashboard/src/utils/colors.ts
@@ -0,0 +1,6 @@
+// Tyres and similar "heat" components
+export const COLD = '#3b82f6'
+export const COLD_WARM = '#86efac'
+export const WARM = '#22c55e'
+export const WARM_HOT = '#f97316'
+export const HOT = '#ef4444'
