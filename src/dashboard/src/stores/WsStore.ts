2fb6f2b15b4bfef0ae5fc293be38a3e0a8577802
Manuel Waibel <manuel-waibel@gmx.at>
2026-06-09 10:20:35 +0200

    Harden store types and add tyre compound enum mapper in TyreStore

--- src/dashboard/src/stores/WsStore.ts
+++ src/dashboard/src/stores/WsStore.ts
@@ -1,12 +1,19 @@
-import { defineStore } from "pinia"
+import type { WsConnectionStatus } from '@/types/store/wsConnectionStatus'
+import { defineStore } from 'pinia'
 
-export const useWsStore = defineStore("wsStore", {
-  state: () => ({
-    wsConnected: false,
-  }),
+export const useWsStore = defineStore('wsStore', {
+  state: () =>
+    ({
+      wsConnected: false,
+    }) as WsConnectionStatus,
   actions: {
+    /**
+     * Set the WebSocket connection status.
+     *
+     * @param {boolean} connected - The new WebSocket connection status.
+     */
     setWsConnected(connected: boolean) {
       this.wsConnected = connected
-    }
-  }
+    },
+  },
 })
