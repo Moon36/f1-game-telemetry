4da7120ab12acea89da73f7ac247aa3578473af0
Manuel Waibel <manuel-waibel@gmx.at>
2026-06-09 11:29:16 +0200

    Use interface field

--- src/dashboard/src/services/parsers/telemetryParser23.ts
+++ src/dashboard/src/services/parsers/telemetryParser23.ts
@@ -6,7 +6,7 @@ import type { M_carTelemetry, M_carStatusData } from '@/types/packets/v23/packet
 
 export class Parser23 implements TelemetryParser<RawTelemetry23> {
   parseCarTelemetry(rawData: RawTelemetry23, carIndex: number): CarData {
-    const carData = rawData['M_carTelemetry']?.[carIndex] as M_carTelemetry | undefined
+    const carData = rawData.M_carTelemetry?.[carIndex] as M_carTelemetry | undefined
     return {
       innerTyreTemps: carData?.M_tyresInnerTemperature || [0, 0, 0, 0],
       outerTyreTemps: carData?.M_tyresSurfaceTemperature || [0, 0, 0, 0],
@@ -14,7 +14,7 @@ export class Parser23 implements TelemetryParser<RawTelemetry23> {
   }
 
   parseCarStatusTelemetry(rawData: RawTelemetry23, carIndex: number): CarStatusData {
-    const statusData = rawData['M_carStatusData']?.[carIndex] as M_carStatusData | undefined
+    const statusData = rawData.M_carStatusData?.[carIndex] as M_carStatusData | undefined
     return {
       actualTyreCompoundId: statusData?.M_actualTyreCompound ?? 0,
     }
